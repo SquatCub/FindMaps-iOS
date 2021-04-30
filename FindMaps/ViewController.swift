2ef02143a393a00783d51c3096e78e8991d5119a
squatcub <brandon.alexis10@hotmail.com>
2021-04-30 15:25:46 -0500

    Correccion en la barra de busqueda, ya es funcional

--- FindMaps/ViewController.swift
+++ FindMaps/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        buscadorSearchBar.delegate = self
         manager.delegate = self
         manager.requestWhenInUseAuthorization()
         manager.requestLocation()
