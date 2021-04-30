88ee54b1fe457083eca2c96c340a872a398d4fbd
squatcub <brandon.alexis10@hotmail.com>
2021-04-30 15:19:12 -0500

    Avance para obtener la ubicacion por la barra de busqueda, aun no funciona el submit

--- FindMaps/ViewController.swift
+++ FindMaps/ViewController.swift
@@ -44,11 +44,20 @@ class ViewController: UIViewController {
         
         alerta.addAction(accionAceptar)
         present(alerta, animated: true)
+        
+        let localizacion = CLLocationCoordinate2D(latitude: latitud!, longitude: longitud!)
+        
+        let spanMapa = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        let region = MKCoordinateRegion(center: localizacion, span: spanMapa)
+        
+        MapMK.setRegion(region, animated: true)
+        MapMK.showsUserLocation = true
     }
     
 }
 
-extension ViewController: CLLocationManagerDelegate {
+extension ViewController: CLLocationManagerDelegate, UISearchBarDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let ubicacion = locations.first else {
             return
@@ -60,5 +69,28 @@ extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error al obtener la ubicacion \(error)")
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        buscadorSearchBar.resignFirstResponder()
+        
+       let geocoder = CLGeocoder()
+        
+        if let direccion = buscadorSearchBar.text {
+            geocoder.geocodeAddressString(direccion) { (places: [CLPlacemark]?, error: Error?) in
+                if error == nil {
+                    let lugar = places?.first
+                    let anotacion = MKPointAnnotation()
+                    anotacion.coordinate = (lugar?.location?.coordinate)!
+                    anotacion.title = direccion
+                    let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                    let region = MKCoordinateRegion(center: anotacion.coordinate, span: span)
+                    
+                    self.MapMK.setRegion(region, animated: true)
+                    self.MapMK.addAnnotation(anotacion)
+                    self.MapMK.selectAnnotation(anotacion, animated: true)
+                }
+            }
+        }
     }
 }
